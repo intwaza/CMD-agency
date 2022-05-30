b6efd0332c8916812fe45a50419f1d0194f01ca0
intwaza <iintwazabelyse@gmail.com>
2022-05-30 20:13:37 +0300

    sorting algorithm

---
+++ read.py
@@ -0,0 +1,11 @@
+import csv
+
+file = open("real-estate.csv")
+csvreader = csv.reader(file)
+header = next(csvreader)
+print(header)
+rows = []
+for row in csvreader:
+    rows.append(row)
+print(rows)
+file.close()
